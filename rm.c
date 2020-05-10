04fbdd4d0c320e52fe3f0e76076a6850969aee34
Dimas Pramudya Haqqi <55182072+dimasssph@users.noreply.github.com>
2020-05-10 17:55:13 +0700

    Update rm

--- rm.c
+++ rm.c
@@ -1,10 +1,59 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include "fcntl.h"
+#include "fs.h"
 
-int
-main(int argc, char *argv[])
-{
+void rm_one(char *s) {
+	if (unlink(s) < 0) {
+		printf(2, "rm: %s failed to delete\n", s);
+		return;
+	}
+}
+
+void rm_recurse(char *dir) {
+	int fd;
+	char buf[512], *p;
+	struct dirent de;
+	struct stat st;
+
+	fd = open(dir, O_RDONLY);
+
+	fstat(fd, &st);
+
+	switch (st.type) {
+		case T_FILE:
+			rm_one(dir);
+			break;
+		case T_DIR:
+			strcpy(buf, dir);
+			p = buf+strlen(buf);
+			*p = '/';
+			p++;
+
+			while(read(fd, &de, sizeof(de)) == sizeof(de)) {
+				if(de.inum == 0 || !strcmp(de.name, ".") || !strcmp(de.name, ".."))
+					continue;
+
+				memmove(p, de.name, strlen(de.name));
+				p[strlen(de.name)] = 0;
+
+				fstat(open(buf, O_RDONLY), &st);
+				if (st.type == T_FILE) 
+					rm_one(buf);
+				else if (st.type == T_DIR)
+					rm_recurse(buf);
+			}
+
+			break;
+	}
+
+	unlink(dir);
+
+	close(fd);
+}
+
+int main(int argc, char *argv[]){
   int i;
 
   if(argc < 2){
@@ -12,12 +61,16 @@ main(int argc, char *argv[])
     exit();
   }
 
-  for(i = 1; i < argc; i++){
-    if(unlink(argv[i]) < 0){
-      printf(2, "rm: %s failed to delete\n", argv[i]);
-      break;
-    }
-  }
+  if (!strcmp(argv[1], "-r")) {
+	  for (i = 2; i < argc; i++) {
+			rm_recurse(argv[2]);
+		}
+	}
+  else {
+		for(i = 1; i < argc; i++){
+			rm_one(argv[i]);
+		}
+	}
 
-  exit();
+	exit();
 }
