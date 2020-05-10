69e68d0c0d31b3d1d266920c1e49a9069f2285dc
Dimas Pramudya Haqqi <55182072+dimasssph@users.noreply.github.com>
2020-05-10 17:53:57 +0700

    New cd

--- cd.c
+++ cd.c
@@ -1,15 +1,24 @@
-#include "unistd.h"
-#include "stdio.h"
-#include "stdlib.h"
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
 
-int main(int argc,char *argv[])
+
+
+int main(int argc, char *argv[])
 {
-    if(argc<2)
-    {
-        exit(0);
-    }
-    if(chdir(argv[1])!= 0)
-    {
-        exit(0);
-    }
+  //mengecek banyaknya paramater yang akan dieksekusi
+  if(argc < 2) //terpenuhi jika command cd dipanggil tidak terdapat destinasi path yang dituju
+  {
+    printf(1, "cd need arguments [path destination]\n");
+    exit();
+  }
+
+  //mengecek isi dari argv[1] apakah ada atau tidaknya path yg dituju
+  if(chdir(argv[1]) < 0) //jika mengeluarkan nilai negatif, maka muncul pesan error
+  {
+    printf(1, "Failed to change directory to *s!\n", argv[1]);
+  }
+  exit(); // jika kondisi diatas tidak ada yg terpenuhi maka cd di eksekusi dan program exit
+
 }
