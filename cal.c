b998cb19932390e9c0de66872649018cfb20ef80
Dimas Pramudya Haqqi <55182072+dimasssph@users.noreply.github.com>
2020-05-10 17:56:11 +0700

    Update cal.c

--- cal.c
+++ cal.c
@@ -1,140 +1,92 @@
 #include "types.h"
-#include "defs.h"
-#include "param.h"
-#include "memlayout.h"
-#include "mmu.h"
-#include "proc.h"
-#include "x86.h"
-#include "syscall.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
 
-// User code makes a system call with INT T_SYSCALL.
-// System call number in %eax.
-// Arguments on the stack, from the user call to the C
-// library system call function. The saved user %esp points
-// to a saved program counter, and then the first argument.
+#define TRUE    1
+#define FALSE   0
 
-// Fetch the int at addr from the current process.
-int
-fetchint(uint addr, int *ip)
+int days_in_month[]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+char *months[]=
 {
-  if(addr >= proc->sz || addr+4 > proc->sz)
-    return -1;
-  *ip = *(int*)(addr);
-  return 0;
-}
-
-// Fetch the nul-terminated string at addr from the current process.
-// Doesn't actually copy the string - just sets *pp to point at it.
-// Returns length of string, not including nul.
-int
-fetchstr(uint addr, char **pp)
-{
-  char *s, *ep;
-
-  if(addr >= proc->sz)
-    return -1;
-  *pp = (char*)addr;
-  ep = (char*)proc->sz;
-  for(s = *pp; s < ep; s++)
-    if(*s == 0)
-      return s - *pp;
-  return -1;
-}
-
-// Fetch the nth 32-bit system call argument.
-int
-argint(int n, int *ip)
-{
-  return fetchint(proc->tf->esp + 4 + 4*n, ip);
-}
-
-// Fetch the nth word-sized system call argument as a pointer
-// to a block of memory of size n bytes.  Check that the pointer
-// lies within the process address space.
-int
-argptr(int n, char **pp, int size)
-{
-  int i;
-  
-  if(argint(n, &i) < 0)
-    return -1;
-  if((uint)i >= proc->sz || (uint)i+size > proc->sz)
-    return -1;
-  *pp = (char*)i;
-  return 0;
-}
-
-// Fetch the nth word-sized system call argument as a string pointer.
-// Check that the pointer is valid and the string is nul-terminated.
-// (There is no shared writable memory, so the string can't change
-// between this check and being used by the kernel.)
-int
-argstr(int n, char **pp)
-{
-  int addr;
-  if(argint(n, &addr) < 0)
-    return -1;
-  return fetchstr(addr, pp);
-}
-
-extern int sys_chdir(void);
-extern int sys_close(void);
-extern int sys_dup(void);
-extern int sys_exec(void);
-extern int sys_exit(void);
-extern int sys_fork(void);
-extern int sys_fstat(void);
-extern int sys_getpid(void);
-extern int sys_kill(void);
-extern int sys_link(void);
-extern int sys_mkdir(void);
-extern int sys_mknod(void);
-extern int sys_open(void);
-extern int sys_pipe(void);
-extern int sys_read(void);
-extern int sys_sbrk(void);
-extern int sys_sleep(void);
-extern int sys_unlink(void);
-extern int sys_wait(void);
-extern int sys_write(void);
-extern int sys_uptime(void);
-extern int sys_waitx(void);
-static int (*syscalls[])(void) = {
-[SYS_fork]    sys_fork,
-[SYS_exit]    sys_exit,
-[SYS_wait]    sys_wait,
-[SYS_pipe]    sys_pipe,
-[SYS_read]    sys_read,
-[SYS_kill]    sys_kill,
-[SYS_exec]    sys_exec,
-[SYS_fstat]   sys_fstat,
-[SYS_chdir]   sys_chdir,
-[SYS_dup]     sys_dup,
-[SYS_getpid]  sys_getpid,
-[SYS_sbrk]    sys_sbrk,
-[SYS_sleep]   sys_sleep,
-[SYS_uptime]  sys_uptime,
-[SYS_open]    sys_open,
-[SYS_write]   sys_write,
-[SYS_mknod]   sys_mknod,
-[SYS_unlink]  sys_unlink,
-[SYS_link]    sys_link,
-[SYS_mkdir]   sys_mkdir,
-[SYS_close]   sys_close,
-[SYS_waitx]   sys_waitx,
+	" ",
+	"\n\n\nJanuary",
+	"\n\n\nFebruary",
+	"\n\n\nMarch",
+	"\n\n\nApril",
+	"\n\n\nMay",
+	"\n\n\nJune",
+	"\n\n\nJuly",
+	"\n\n\nAugust",
+	"\n\n\nSeptember",
+	"\n\n\nOctober",
+	"\n\n\nNovember",
+	"\n\n\nDecember"
 };
 
-void
-syscall(void)
+int determinedaycode(int year)
 {
-  int num;
+	int daycode;
+	int d1, d2, d3;
+	
+	d1 = (year - 1.)/ 4.0;
+	d2 = (year - 1.)/ 100.;
+	d3 = (year - 1.)/ 400.;
+	daycode = (year + d1 - d2 + d3) %7;
+	return daycode;
+}
 
-  num = proc->tf->eax;
-  if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
-    proc->tf->eax = syscalls[num]();
-  } else {
-    cprintf("%d %s: unknown sys call %d\n",
-            proc->pid, proc->name, num);
-    proc->tf->eax = -1;
-  }
+
+int determineleapyear(int year)
+{
+	if((year% 4 == FALSE && year%100 != FALSE) || year%400 == FALSE)
+	{
+		days_in_month[2] = 29;
+		return TRUE;
+	}
+	else
+	{
+		days_in_month[2] = 28;
+		return FALSE;
+	}
+}
+
+void calendar(int year, int daycode)
+{
+	int month, day;
+	for ( month = 1; month <= 12; month++ )
+	{
+		printf(1,"%s", months[month]);
+		printf(1,"\n\nSun  Mon  Tue  Wed  Thu  Fri  Sat\n" );
+		
+		// Correct the position for the first date
+		for ( day = 1; day <= 1 + daycode * 5; day++ )
+		{
+			printf(1," ");
+		}
+		
+		// Print all the dates for one month
+		for ( day = 1; day <= days_in_month[month]; day++ )
+		{
+			printf(1,"%d", day );
+			
+			// Is day before Sat? Else start next line Sun.
+			if ( ( day + daycode ) % 7 > 0 )
+				printf(1,"   " );
+			else
+				printf(1,"\n " );
+		}
+			// Set position for next month
+			daycode = ( daycode + days_in_month[month] ) % 7;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	int year, daycode;
+	year = atoi(argv[1]);
+	daycode = determinedaycode(year);
+	determineleapyear(year);
+	calendar(year, daycode);
+	printf(1,"\n");
 }
